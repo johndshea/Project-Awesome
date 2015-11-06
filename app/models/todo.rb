a4378cd9a69af2e543f1d145da95caae786bf842
John Shea <john@johnshea.nyc>
2015-11-06 12:33:10 -0500

    added find_users and find_teams methods to the Todo class

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,25 @@
 class Todo < ActiveRecord::Base
 	has_many :users, as: :todo_assignment
 	has_many :teams, as: :todo_assignment
+
+	def find_users
+		User.find_each do |user|
+			user.todos.map do |todo|
+				if todo.id == self.id
+					return user
+				end
+			end
+		end
+	end
+
+	def find_teams
+		Team.find_each do |team|
+			team.todos.map do |todo|
+				if todo.id == self.id
+					return team
+				end
+			end
+		end
+	end
+
 end
