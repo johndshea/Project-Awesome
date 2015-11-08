76cdaa7f0faf84c1a81cc50697912e4be986e6da
John Shea <john@johnshea.nyc>
2015-11-07 19:50:07 -0500

    branching off john_sins

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources :users
   # resources :todos
   root 'todo#todo'
-  resources :todos, only: [:index, :create], defaults: { format: :json } 
+  resources :todos, only: [:index, :create], defaults: { format: :json }
+
   # root 'todo#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
