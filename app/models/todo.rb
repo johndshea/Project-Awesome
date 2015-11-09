a03ddca56c85e65af4a5aab685954b7bbe9c224f
John Shea <john@johnshea.nyc>
2015-11-09 15:46:04 -0500

    fixed database problems with Shorty

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,30 +1,7 @@
 class Todo < ActiveRecord::Base
 	has_many :todo_assignments
-	has_many :users, through: :todo_assignments
+	has_many :users, through: :todo_assignments, source: :assignable, source_type: 'User'
+	has_many :teams, through: :todo_assignments, source: :assignable, source_type: 'Team'
 
 	validates :name, presence: true
-
-	def find_users
-		users = []
-		User.find_each do |user|
-			 user.todos.map do |todo|
-				if todo.id == self.id
-					users.push(user)
-				end
-			end.inject(:+)
-		end
-		return users
-	end
-
-	def find_teams
-		teams = []
-		Team.find_each do |team|
-			team.todos.map do |todo|
-				if todo.id == self.id
-					teams.push(team)
-				end
-			end.inject(:+)
-		end
-	end
-
 end
