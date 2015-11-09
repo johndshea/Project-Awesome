ec2ea46f65636c51077353f47aa25ffd2ec769b2
jonschwadron <jonschwadron@gmail.com>
2015-11-09 14:16:46 -0500

    update

---
+++ app/assets/javascripts/main.coffee.js
@@ -0,0 +1,11 @@
+var app = angular.module("Tasks", []);
+
+app.controller("TasksControl", function($scope, $http) {
+  $http.get('data/tasks.json').
+    success(function(data, status, headers, config) {
+      $scope.tasks = data;
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
+});
