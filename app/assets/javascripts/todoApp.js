f2eb96c41fef61b793517a0fbd1ed1e9a19203b5
Dillon McGuire <DMMcGuire6724@gmail.com>
2015-11-10 16:17:34 -0500

    Weather and Todo controllers fixed

--- app/assets/javascripts/todoApp.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('ToDoApp', ['ngDialog']);
-
-app.controller('MainCtrl', function ($scope, ngDialog) {
-    $scope.clickToOpen = function () {
-        ngDialog.open({ template: 'main/index.html' });
-    };
-});
