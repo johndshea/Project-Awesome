1315a79f743bbe7784b8d3e9b69a1afd5bb42b76
jonschwadron <jonschwadron@gmail.com>
2015-11-10 11:35:57 -0500

    added ng-controller for Todos

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -1,2 +1,11 @@
-//application controllers to go here
-console.log('HEREEEE!!!!!!')
+
+var app = angular.module("Todos", []);
+
+app.controller("TodosControl", function($scope, $http) {
+  $http.get('/todos.json').success(function(data, status, headers, config) {
+      console.log(data);
+      $scope.todos = data;
+    }).error(function(data, status, headers, config) {
+      // log error
+    });
+});
